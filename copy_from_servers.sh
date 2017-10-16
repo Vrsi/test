e1f03764007b264b43760d31a14142e897a04ea6
Vrsi <Vrsa2@seznam.cz>
2017-10-16 14:49:55 +0200

    add

---
+++ copy_from_servers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+while read server; do
+    scp $server:/mnt/data/indexes/wikipedia/slave_server_xvrsas00/list_of_uris /home/xvrsas00/Dokumenty/BP_testy
+    mv /home/xvrsas00/Dokumenty/BP_testy/list_of_uris /home/xvrsas00/Dokumenty/BP_testy/list_of_uris_$server
+done < servers.txt
