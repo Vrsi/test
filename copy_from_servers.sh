4eb5699c892d8486a766920cade20793f1f33f50
Vrsi <Vrsa2@seznam.cz>
2017-10-20 11:03:10 +0200

    test

--- copy_from_servers.sh
+++ copy_from_servers.sh
@@ -4,3 +4,4 @@ while read server; do
     scp $server:/mnt/data/indexes/wikipedia/slave_server_xvrsas00/list_of_uris /home/xvrsas00/Dokumenty/BP_testy
     mv /home/xvrsas00/Dokumenty/BP_testy/list_of_uris /home/xvrsas00/Dokumenty/BP_testy/list_of_uris_$server
 done < servers.txt
+#sss
