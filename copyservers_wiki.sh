74e4f9cb3a163e79bd8b17fe7e4415fad20b3e81
Vrsi <Vrsa2@seznam.cz>
2017-10-23 15:02:24 +0200

    test5

--- copyservers_wiki.sh
+++ copyservers_wiki.sh
@@ -3,4 +3,4 @@
 while read server; do
     scp -r /home/xvrsas00/Dokumenty/CP_na_servery/slave_server_xvrsas00 $server:/mnt/data/indexes/wikipedia
 done < servers.txt
-# zkousim test2 test3
+# zkousim test2 test33
