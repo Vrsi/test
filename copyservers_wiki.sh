9893acd2fee08ba7d410140b94187095f48faaa8
Vrsi <Vrsa2@seznam.cz>
2017-10-25 16:13:56 +0200

    sad

--- copyservers_wiki.sh
+++ copyservers_wiki.sh
@@ -3,5 +3,12 @@
 while read server; do
     scp -r /home/xvrsas00/Dokumenty/CP_na_servery/slave_server_xvrsas00 $server:/mnt/data/indexes/wikipedia
 done < servers.txt
+<<<<<<< Updated upstream
 # zmena zmena test1
 # dalski
+=======
+# zkousim test2 test33
+# test merge
+# CO TU BUDE?
+#sss
+>>>>>>> Stashed changes
