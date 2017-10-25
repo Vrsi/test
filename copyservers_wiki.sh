0305bc9c20e352950121ff1c747ddd79c68f87aa
Vrsi <Vrsa2@seznam.cz>
2017-10-25 19:17:00 +0200

    asdfa

--- copyservers_wiki.sh
+++ copyservers_wiki.sh
@@ -2,5 +2,33 @@
 
 while read server; do
     scp -r /home/xvrsas00/Dokumenty/CP_na_servery/slave_server_xvrsas00 $server:/mnt/data/indexes/wikipedia
+<<<<<<< Updated upstream
 done < servers.txtasdada
+<<<<<<< Updated upstream
 asd
+=======
+=======
+<<<<<<< Updated upstream
+done < servers.txtasdad
+=======
+done < servers.txt
+<<<<<<< Updated upstream
+# zmena zmena test1
+# dalski
+=======
+# zkousim test2 test33
+# test merge
+# CO TU BUDE?
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+#sss
+>>>>>>> Stashed changes
+=======
+#sss
+>>>>>>> Stashed changes
+=======
+#sss
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
